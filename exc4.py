9c8530dff1d6591afb919540c7ec345c73dcd254
Alejandro Batista <ajbatistav@gmail.com>
2020-09-04 22:35:50 -0500

    agregando comentario a exc8.py

--- exc4.py
+++ exc4.py
@@ -2,4 +2,5 @@ numero_a = input("Introduzca el primer numero: ")
 numero_b = input("Introduzca el segundo numero: ")
 numero_a =float(numero_a)
 numero_b =float(numero_b)
+
 print(numero_a > numero_b)
--- exc8.py
+++ exc8.py
@@ -1,9 +1,11 @@
+#variables de entrada
 monto = input ("Introduzca monto prestado: ")
 monto = float(monto)
 cuotas = input ("Introduzca cantidad de cuotas: ")
 cuotas = int(cuotas)
 interes= input ("Introduzca porcentaje de interes: ")
 interes = float(interes) / 100
+#calculo
 ex_1 = (1 + interes) ** cuotas
 ex_2 = ( interes * ex_1) / (ex_1 - 1)
 resultado = monto * ex_2
